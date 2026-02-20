edc9bbba295a13a041e92350c8ea705f049b38f4
SiamAlSobari <siamqueen52@gmail.com>
2026-02-20 13:28:46 +0700

    feat(auth): implement user registration and login functionality
    - Added User model to Prisma schema for user management.
    - Created auth service for handling user registration and login logic.
    - Implemented validation for login and registration using Zod.
    - Integrated JWT token generation for user authentication.
    - Updated auth controller to handle login and registration routes.
    - Created user repository for database interactions related to users.
    - Added standard validator for request validation in Hono framework.

---
+++ src/modules/auth/auth.validation.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const registerAuthValidation = z.object({
+  email: z.email(),
+  password: z
+    .string({ message: "Password wajib di isi" })
+    .min(6, { message: "Minimal 6 karakter" }),
+});
+
+export const loginAuthValidation = z.object({
+  email: z.email(),
+  password: z
+    .string({ message: "Password wajib di isi" })
+    .min(6, { message: "Minimal 6 karakter" }),
+});
