aff2b739d990375e2c1fa92ae09b4d7484b9a355
Firegame224 <xg474627@gmail.com>
2026-02-24 01:12:07 +0700

    refactor: misahin Groupper ke utils supaya lebih enak dibaca

--- src/modules/reflection/reflection.service.ts
+++ src/modules/reflection/reflection.service.ts
@@ -1,12 +1,10 @@
 import { HTTPException } from "hono/http-exception";
-import { reflectionFormatter } from "../../common/utils/reflection.js";
+import { reflectionFormatter, ReflectionGroupper } from "../../common/utils/reflection.js";
 import type { UserRepository } from "../user/user.repository.js";
 import type { ReflectionRepository } from "./reflection.repository.js";
-import { Prisma, QuestLevel, QuestReflectionType, type Quest } from "@prisma/client";
+import { QuestLevel, QuestReflectionType, type Quest } from "@prisma/client";
 import type { QuestRepository } from "../quest/quest.repository.js";
 import type { FolderRepository } from "../folder/folder.repository.js";
-
-type QuestFolderType = Prisma.QuestFolderGetPayload<{ include: { quests: { include: { reflection: true } } } }>
 
 export class ReflectionService {
     constructor(
@@ -15,44 +13,6 @@ export class ReflectionService {
         private readonly reflectionRepository: ReflectionRepository,
         private readonly userRepository: UserRepository
     ) { }
-
-    private ReflectionGroupper(reflectionData: QuestFolderType[]) {
-
-        return reflectionData.flatMap((folder) =>
-            folder.quests.map((quest) => {
-                const diffMs = quest.completedAt && quest.createdAt
-                    ? (new Date(quest.completedAt).getTime() - new Date(quest.createdAt).getTime()) / 1000 / 60
-                    : null
-                const minutes = (diffMs! / 1000 / 60);
-                const cleanMinute = Math.max(1, Math.round(minutes));
-                return {
-                    folder: folder.name,
-                    isSuccess: quest.isSuccess,
-                    deadline: quest.deadLineAt,
-                    completedAt: quest.completedAt,
-                    estimatedMin: cleanMinute,
-                    reflections: quest.reflection
-                }
-            })
-        );
-    }
-
-    //     export function ChatGrouper(data: { chat: Chat, user: AliasType }[]) {
-    //     return data.reduce((acc, data) => {
-    //         const dateKey = format(new Date(data?.chat?.createdAt!), "MM/dd/yyyy");
-
-    //         if (!acc[dateKey]) {
-    //             acc[dateKey] = [];
-    //         }
-
-    //         acc[dateKey].push({
-    //             chat: data.chat!,
-    //             user: data.user
-    //         });
-
-    //         return acc;
-    //     }, {} as Record<string, { chat: Chat; user: AliasType }[]>)
-    // }
 
     async GetLatestReflection(userId: string) {
         const existingUser = await this.userRepository.findUserById(userId)
@@ -89,7 +49,7 @@ export class ReflectionService {
         const startPeriod = new Date(endPeriod.getTime() - 7 * 24 * 60 * 60 * 1000);
         const existingQuest = await this.folderRepository.findUserFolderWithQuestReflection(userId, startPeriod, endPeriod)
 
-        const groupedResult = this.ReflectionGroupper(existingQuest)
+        const groupedResult = ReflectionGroupper(existingQuest)
 
         return groupedResult
     }
