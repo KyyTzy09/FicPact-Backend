fce576abc6dbc3341145c6b9de3f6f188a62e00f
Firegame224 <xg474627@gmail.com>
2026-03-07 01:09:13 +0700

    refactor(job): refine method reflection trigger untuk performa yang lebih baik

---
+++ src/modules/job/job.service.ts
@@ -0,0 +1,20 @@
+import type { ReflectionRepository } from "../reflection/reflection.repository.js";
+import type { UserRepository } from "../user/user.repository.js";
+
+export class JobService {
+    constructor(private readonly userRepository: UserRepository, private readonly reflectionRepository: ReflectionRepository) { }
+
+    public async reflectionTrigger() {
+        const now = new Date()
+        const sevenDaysAgo = new Date(now)
+        sevenDaysAgo.setDate(now.getDate() - 7)
+        // Cari semua User dengan LTE last reflection
+        const users = await this.userRepository.findUsersByLastReflectionDate(sevenDaysAgo)
+        const userIds = users.map((user) => user.id)
+
+        const trigger = await this.reflectionRepository.createManyReflectionTrigger(userIds)
+        const updateLastReflection = await this.userRepository.updateUsersLastReflection(userIds, now)
+
+        return { trigger, updateLastReflection }
+    }
+}
