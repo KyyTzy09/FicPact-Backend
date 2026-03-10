b6d9585542d8939c3942e286a31e0f6c65ed371e
Firegame224 <xg474627@gmail.com>
2026-03-10 23:48:14 +0700

    fix: mengubah user repository method terkait token, fix(auth): mengubah url reset-password untuk redirect ke url frontend

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { hashPassword, otpGenerator, tokenGenerator, verifyPassword } from "../.
 import { FRONTEND_BASE_URL, JWT_SECRET } from "../../common/utils/env.js";
 import { generateToken } from "../../common/utils/jwt.js";
 import { sendEmail } from "../../common/utils/email.js";
+import { VerificationTokenType } from "@prisma/client";
 
 export class AuthService {
   constructor(
@@ -22,7 +23,7 @@ export class AuthService {
     const verificationToken = await otpGenerator(6);
     const token = await generateToken(payload, JWT_SECRET);
 
-    await this.userRepository.updateUserVerificationToken(create.id, verificationToken, new Date(Date.now() + 1000 * 60 * 15))
+    await this.userRepository.createToken(create.id, verificationToken, new Date(Date.now() + 1000 * 60 * 15), VerificationTokenType.ACCOUNT)
     await this.sendEmailVerificationToken(email, verificationToken)
     return { token, create };
   }
@@ -51,9 +52,9 @@ export class AuthService {
     if (!existingUser) throw new HTTPException(404, { message: "User tidak ditemukan" })
 
     const { rawToken, hashedToken } = await tokenGenerator();
-    const updatedUserToken = await this.userRepository.updateResetPassToken(email, hashedToken, new Date(Date.now() + 1000 * 60 * 15)) // 15 Minutes
+    const updatedUserToken = await this.userRepository.createToken(existingUser.id, hashedToken, new Date(Date.now() + 1000 * 60 * 15), VerificationTokenType.PASSWORD) // 15 Minutes(email, hashedToken, new Date(Date.now() + 1000 * 60 * 15)) // 15 Minutes
 
-    const resetLink = `${FRONTEND_BASE_URL}/auth/reset-password?token=${rawToken}&email=${email}`
+    const resetLink = `${FRONTEND_BASE_URL}/auth/change-password?token=${rawToken}&email=${email}`
     const html = `<h2>Reset Password</h2>
     <p>Klik link di bawah untuk reset password:</p>
     <a href="${resetLink}">${resetLink}</a>
@@ -84,12 +85,13 @@ export class AuthService {
     const existingUser = await this.userRepository.findUserByEmail(email || "")
     if (!existingUser) throw new HTTPException(404, { message: "User tidak ditemukan" })
 
-    // Cek apakah token sudah expired
-    const isTokenExpired = !existingUser?.resetPasswordExpiry || existingUser.resetPasswordExpiry < new Date()
-    if (!existingUser || isTokenExpired) throw new HTTPException(404, { message: "User tidak ditemukan atau token sudah expired" })
+
+    // Cek apakah token ada
+    const existingToken = await this.userRepository.findLatestToken(existingUser.id, VerificationTokenType.PASSWORD, new Date())
+    if (!existingToken) throw new HTTPException(404, { message: "User tidak ditemukan atau token sudah expired" })
 
     // Cek apakah token cocok dengan yang di database
-    const isTokenMatch = await verifyPassword(existingUser.resetPasswordToken || "", token)
+    const isTokenMatch = await verifyPassword(existingToken.code || "", token)
     if (!isTokenMatch) throw new HTTPException(400, { message: "Token salah" })
 
     // Hash password baru dan update ke database
@@ -99,22 +101,26 @@ export class AuthService {
   }
 
   public async verifyAccount(userId: string, token: string) {
-    const existingUser = await this.userRepository.findUserWithVerifyTokenExpiry(userId, new Date());
+    const existingUser = await this.userRepository.findUserById(userId);
     if (!existingUser) throw new HTTPException(404, { message: "User token tidak ditemukan atau sudah kadaluarsa" })
 
-    const isTokenMatched = await existingUser.verificationToken === token
+    const existingToken = await this.userRepository.findUserVerificationToken(existingUser.id, token, VerificationTokenType.ACCOUNT, new Date())
+    if (!existingToken) throw new HTTPException(404, { message: "User token tidak ditemukan atau sudah kadaluarsa" })
+
+    const isTokenMatched = existingToken.code === token
     if (!isTokenMatched) throw new HTTPException(400, { message: "Token salah" })
 
     const verifiedUser = await this.userRepository.verifyUser(existingUser.id)
     return { verifiedUser }
   }
 
+
   public async resendVerificationToken(userId: string) {
     const existingUser = await this.userRepository.findUserById(userId);
     if (!existingUser) throw new HTTPException(404, { message: "User tidak ditemukan" })
 
     const token = await otpGenerator(6);
-    const updatedUserToken = await this.userRepository.updateUserVerificationToken(existingUser.id, token, new Date(Date.now() + 1000 * 60 * 15))
+    const updatedUserToken = await this.userRepository.createToken(existingUser.id, token, new Date(Date.now() + 1000 * 60 * 15), VerificationTokenType.ACCOUNT)
 
     const html = `<h2>Verifikasi Akun</h2>
         <p>Masukan token berikut untuk verifikasi akun kamu:</p>
