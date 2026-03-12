739d2971f8644b3a284abe12dda865547e5dcbf9
Firegame224 <xg474627@gmail.com>
2026-03-12 21:51:54 +0700

    feat(ai): menambahkan service untuk createQuestAI dan memisahkan payload & response type ai service

---
+++ src/common/external/ai.type.ts
@@ -0,0 +1,35 @@
+import type { Prisma, QuestLevel, QuestReflectionType } from "@prisma/client"
+
+export type AIReflectionPayload = {
+  folder: string
+  isSuccess: boolean
+  deadline: Date
+  completedAt: Date | null
+  estimatedMin: number
+  reflections: {
+    type: QuestReflectionType
+    reason: string
+    questLevel: QuestLevel
+  }[]
+}
+
+export type AICreateQuestPayload = {
+  title: string
+  folders: Prisma.QuestFolderGetPayload<{ include: { quests: true } }>
+  create_folder: boolean
+}
+
+export type AICreateQuestResponse = {
+  action: "create_folder_with_quests" | "create_quests_only"
+  folderId?: string
+  folder?: {
+    name: string
+    description: string | null
+  }
+  quests: {
+    name: string
+    description: string | null
+    expReward: number
+    deadLineAt: Date
+  }[]
+}
--- src/modules/ai/ai.service.ts
+++ src/modules/ai/ai.service.ts
@@ -1,68 +1,37 @@
-import type { QuestLevel, QuestReflectionType } from "@prisma/client"
+import type { Prisma, QuestLevel, QuestReflectionType } from "@prisma/client"
 import { AI_API_BASE_URL, AI_API_TOKEN } from "../../common/utils/env.js"
+import type { AICreateQuestPayload, AICreateQuestResponse, AIReflectionPayload } from "../../common/external/ai.type.js";
 
-type AIReflectionPayload = {
-    folder: string
-    isSuccess: boolean
-    deadline: Date
-    completedAt: Date | null
-    estimatedMin: number
-    reflections: {
-        type: QuestReflectionType
-        reason: string
-        questLevel: QuestLevel
-    }[]
-}
 
 export class AIService {
-    async FetchAIReflection(data: AIReflectionPayload[]): Promise<string> {
-        const response = await fetch(`${AI_API_BASE_URL}/reflection`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${AI_API_TOKEN}`,
-            },
-            body: JSON.stringify({
-                histories: data
-            }),
-        });
+  async FetchAIReflection(data: AIReflectionPayload[]): Promise<string> {
+    const response = await fetch(`${AI_API_BASE_URL}/reflection`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${AI_API_TOKEN}`,
+      },
+      body: JSON.stringify({
+        histories: data
+      }),
+    });
 
-        const result = await response.json();
-        return result.data as string
+    const result = await response.json();
+    return result.data as string
 
-    }
+  }
 
-    public async command(text: string) {
-        // nanti di sini data kirim ke AI
-        //
-        //
-        // HARAPAN DATA DARI AI ITU DATA JSON SEPERTI INI
-        //
-        //
-        // {
-        //   "action": "create_folder_with_quests",
-        //   "folder": {
-        //     "name": "Belajar Ulangan",
-        //     "description": "Persiapan ulangan"
-        //   },
-        //   "quests": [
-        //     {
-        //       "name": "Belajar Matematika",
-        //       "description": "Review materi integral",
-        //       "expReward": 20,
-        //       "deadLineAt": "2026-03-20T18:00:00Z"
-        //     },
-        //     {
-        //       "name": "Belajar Fisika",
-        //       "description": "Latihan soal mekanika",
-        //       "expReward": 20,
-        //       "deadLineAt": "2026-03-20T18:00:00Z"
-        //     }
-        //   ]
-        // }
-        //
-        //
-        //
-        // Kirim data ke DB
-    }
+  public async FetchAICreateQuest(payload: AICreateQuestPayload): Promise<AICreateQuestResponse> {
+    const response = await fetch(`${AI_API_BASE_URL}/create-quest`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${AI_API_TOKEN}`,
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const result = await response.json();
+    return result.data
+  }
 }
