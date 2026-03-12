50ee27cb1a1fcde6c210b1b16f3409ebc919fb37
SiamAlSobari <siamqueen52@gmail.com>
2026-03-12 21:12:37 +0700

    ea

--- src/modules/ai/ai.controller.ts
+++ src/modules/ai/ai.controller.ts
@@ -1,5 +1,8 @@
 import { Hono } from "hono";
 import { authMiddleware } from "../../common/middlewares/auth.middleware.js";
+import { AIService } from "./ai.service.js";
+
+const aiService = new AIService()
 
 export const aiController = new Hono()
     .post(
@@ -7,7 +10,7 @@ export const aiController = new Hono()
         authMiddleware,
         async (c) => {
             const { text } = await c.req.json()
-            
+            const res = await aiService.command(text)
             
             // {
             //   "action": "create_folder_with_quests",
