dbc63e730988799472df98e94c7814959b5fdc19
Firegame224 <xg474627@gmail.com>
2026-03-28 02:18:43 +0700

    feat(folder): menambahkan orderBy untuk mengurutkan data quest dari Deadline terkecil

--- src/modules/folder/folder.repository.ts
+++ src/modules/folder/folder.repository.ts
@@ -2,149 +2,173 @@ import type { FolderStatus } from "@prisma/client";
 import { prisma } from "../../common/utils/prisma.js";
 
 export class FolderRepository {
-    public async findFolderByName(userId: string, name: string) {
-        return await prisma.questFolder.findUnique({
-            where: {
-                userId_name: {
-                    userId,
-                    name
-                }
-            }
-        })
-    }
+  public async findFolderByName(userId: string, name: string) {
+    return await prisma.questFolder.findUnique({
+      where: {
+        userId_name: {
+          userId,
+          name,
+        },
+      },
+    });
+  }
 
-    public async findUserFolderWithQuestReflectionByPeriod(userId: string, startDate: Date, endDate: Date) {
-        return await prisma.questFolder.findMany({
-            where: {
-                userId,
-                createdAt: {
-                    lt: endDate,
-                    gte: startDate
-                },
+  public async findUserFolderWithQuestReflectionByPeriod(
+    userId: string,
+    startDate: Date,
+    endDate: Date,
+  ) {
+    return await prisma.questFolder.findMany({
+      where: {
+        userId,
+        createdAt: {
+          lt: endDate,
+          gte: startDate,
+        },
+      },
+      include: {
+        quests: {
+          include: {
+            reflection: {
+              select: {
+                questLevel: true,
+                reason: true,
+                type: true,
+              },
             },
-            include: {
-                quests: {
-                    include: {
-                        reflection: {
-                            select: {
-                                questLevel: true,
-                                reason: true,
-                                type: true
-                            }
-                        }
-                    }
-                }
-            }
-        })
-    }
+          },
+        },
+      },
+    });
+  }
 
-    public async findFolderByUserIdAndId(userId: string, folderId: string) {
-        return await prisma.questFolder.findUnique({
-            where: {
-                userId,
-                id: folderId
-            },
-            include: {
-                quests: true
-            }
-        })
-    }
+  public async findFolderByUserIdAndId(userId: string, folderId: string) {
+    return await prisma.questFolder.findUnique({
+      where: {
+        userId,
+        id: folderId,
+      },
+      include: {
+        quests: {
+          orderBy: {
+            deadLineAt: "asc",
+          },
+        },
+      },
+    });
+  }
 
-    public async findPendingFolderByUserId(userId: string) {
-        return await prisma.questFolder.findMany({
-            where: {
-                userId,
-                status: "PENDING"
-            },
-            include: {
-                quests: true
-            }
-        })
-    }
+  public async findPendingFolderByUserId(userId: string) {
+    return await prisma.questFolder.findMany({
+      where: {
+        userId,
+        status: "PENDING",
+      },
+      include: {
+        quests: true,
+      },
+    });
+  }
 
-    public async findFolderByNames(userId: string, names: string) {
-        return await prisma.questFolder.findMany({
-            where: {
-                userId,
-                name: {
-                    startsWith: names
-                }
-            },
-            select: {
-                name: true
-            }
-        })
-    }
+  public async findFolderByNames(userId: string, names: string) {
+    return await prisma.questFolder.findMany({
+      where: {
+        userId,
+        name: {
+          startsWith: names,
+        },
+      },
+      select: {
+        name: true,
+      },
+    });
+  }
 
-    public async findFolderByUserId(userId: string) {
-        return await prisma.questFolder.findMany({
-            where: {
-                userId
-            },
-            include: {
-                quests: true
-            },
-            orderBy: {
-                name: "asc"
-            }
-        })
-    }
+  public async findFolderByUserId(userId: string) {
+    return await prisma.questFolder.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        quests: {
+          orderBy: {
+            deadLineAt: "asc",
+          },
+        },
+      },
+      orderBy: {
+        name: "asc",
+      },
+    });
+  }
 
-    public async countFoldersByUserId(userId: string) {
-        return await prisma.questFolder.count({
-            where: {
-                userId
-            }
-        })
-    }
+  public async countFoldersByUserId(userId: string) {
+    return await prisma.questFolder.count({
+      where: {
+        userId,
+      },
+    });
+  }
 
-    public async findAvailableFoldersByUserId(userId: string) {
-        return await prisma.questFolder.findMany({
-            where: {
-                userId,
-                status: "PENDING"
-            }
-        })
-    }
+  public async findAvailableFoldersByUserId(userId: string) {
+    return await prisma.questFolder.findMany({
+      where: {
+        userId,
+        status: "PENDING",
+      },
+    });
+  }
 
-    public async updateStatus(folderId: string, status: FolderStatus) {
-        return await prisma.questFolder.update({
-            where: { id: folderId },
-            data: { status: status },
-        });
-    }
+  public async updateStatus(folderId: string, status: FolderStatus) {
+    return await prisma.questFolder.update({
+      where: { id: folderId },
+      data: { status: status },
+    });
+  }
 
-    public async createFolder(userId: string, name: string, endedAt: string, description?: string, icon?: string, color?: string) {
-        return await prisma.questFolder.create({
-            data: {
-                name,
-                description,
-                userId,
-                endedAt,
-                icon,
-                color
-            }
-        })
-    }
+  public async createFolder(
+    userId: string,
+    name: string,
+    endedAt: string,
+    description?: string,
+    icon?: string,
+    color?: string,
+  ) {
+    return await prisma.questFolder.create({
+      data: {
+        name,
+        description,
+        userId,
+        endedAt,
+        icon,
+        color,
+      },
+    });
+  }
 
-    public async updateFolder(folderId: string, userId: string, name: string, description?: string) {
-        return await prisma.questFolder.update({
-            where: {
-                id: folderId,
-                userId
-            },
-            data: {
-                name,
-                description
-            }
-        })
-    }
+  public async updateFolder(
+    folderId: string,
+    userId: string,
+    name: string,
+    description?: string,
+  ) {
+    return await prisma.questFolder.update({
+      where: {
+        id: folderId,
+        userId,
+      },
+      data: {
+        name,
+        description,
+      },
+    });
+  }
 
-    public async deleteFolderById(folderId: string) {
-        return await prisma.questFolder.delete({
-            where: {
-                id: folderId
-            }
-        })
-    }
+  public async deleteFolderById(folderId: string) {
+    return await prisma.questFolder.delete({
+      where: {
+        id: folderId,
+      },
+    });
+  }
 }
